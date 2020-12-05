7d31e3f8e1e1960739d573132f202a376b56ee0d
ishmam ali <ishmamalikhan@gmail.com>
2020-12-05 16:27:28 +0500

    Echo And Print

---
+++ echo.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>PHP</title>
+</head>
+<body>
+<?php
+///echo is used for printing variable or value
+print "<b><i>ishmam"." ali</b></i><br>";
+print 23.4
+
+/// difference between echo and print
+/// in echo we can print more than one stirng value by differntinate , but in print we cannot do this
+// echo is faster than print
+?>
+</body>
+</html>
