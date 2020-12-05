f6819c4ee4d1a3bb5954a134c83deed07be6c275
ishmam ali <ishmamalikhan@gmail.com>
2020-12-05 18:11:49 +0500

    recursive function

---
+++ recursiveFunction.php
@@ -0,0 +1,26 @@
+<?php
+
+
+// function display($number){
+//     if($number<=5){
+//         echo "$number <br>";
+//         display($number +1);
+//     }
+// }
+
+// display(1);
+
+
+
+function factorial($n){
+    if($n==0){
+        return 1;
+    }else{
+        return ($n*factorial($n-1));
+    }
+}
+
+
+echo factorial(5)
+
+?>
