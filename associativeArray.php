6b9d2c442820fe94de6933ffe11e545e39ae0a0c
ishmam ali <ishmamalikhan@gmail.com>
2020-12-05 18:34:42 +0500

    associative Array

---
+++ associativeArray.php
@@ -0,0 +1,22 @@
+<?php
+
+
+/// An Array In Which We Can Give the name of array index is called associative array
+
+
+
+///simple array
+// $a=[10,20,30];
+// echo "<pre>";
+// print_r($a);
+// echo "</pre>";
+
+
+// /// Associative Array
+// $a=array("Bill"=>10,"Joe"=>20,"Peter"=>30);
+// echo $a["Bill"]."<br>";
+// echo $a["Joe"]."<br>";
+// echo $a["Peter"]."<br>";
+
+
+?>
