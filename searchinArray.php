d537f323a49bc728634f9d38f44937047dc3e427
ishmam ali <ishmamalikhan@gmail.com>
2020-12-05 21:43:03 +0500

    searching in Array Function

---
+++ searchinArray.php
@@ -0,0 +1,24 @@
+<?php
+$a=["Ishmam","Ali","Khan",'55',"Mahota"];
+// echo in_array('Ali',$a);
+
+/// This array will return index of array
+echo array_search('55',$a);
+
+
+
+
+///Multidemnsion Search
+// $a=array(
+//     array('p','h'),
+//     array('p','r'),
+//     array('o'),
+// );
+// if(in_array(array('p','r'),$a,true)){
+//     echo "Find SuccessFully";
+// }else{
+//     echo "Failed To Find";
+// }
+
+
+?>
