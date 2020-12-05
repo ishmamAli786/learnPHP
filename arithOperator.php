00e08ee48d9f49ca2a63335b96fe363b12d36f81
ishmam ali <ishmamalikhan@gmail.com>
2020-12-05 17:21:34 +0500

    arithmatic operation

---
+++ arithOperator.php
@@ -0,0 +1,53 @@
+<?php
+
+
+// $a=10.55;
+// $b=20;
+// $c=$a+$b; /// addition operator
+// echo $c;
+
+
+// $a=10;
+// $b=3;
+// $c=$a-$b; /// Subtraction operator
+// echo $c;
+
+// $a=10;
+// $b=3;
+// $c=$a*$b; /// Multiplication operator
+// echo $c;
+
+
+// $a=10;
+// $b=3;
+// $c=$a/$b; /// Division operator
+// echo $c
+
+
+// $a=14;
+// $b=2;
+// $c=$a%$b; /// Modulus operator
+// echo $c
+
+
+// $a=10;
+// $b=3;
+// $c=$a**$b; /// Exponent operator
+// echo $c
+
+
+// $a=10; /// Increment operator
+// $a++;
+// echo $a;
+
+
+// $a=10; /// Decrement operator
+// $a--;
+// echo $a;
+
+
+$a=10; /// InPre Increment operator First Return the value of variable and then increment
+++$a;
+echo $a;
+
+?>
