8412cd8673bfdce2f67fa915720b73ddb8eb8ea9
ishmam ali <ishmamalikhan@gmail.com>
2020-12-05 16:54:24 +0500

    dataTypes

---
+++ dataTypes.php
@@ -0,0 +1,31 @@
+<?php
+// $x="Khan" ;  /// String Type
+// echo $x."<br>";
+// var_dump($x); //// Var_dump function is used for find the value and type of variable also tell character of variable
+
+
+// $y=2500; /// Numarical Type
+// echo $y."<br>";
+// var_dump($y);
+
+
+// $z=2500.50; ///float Type
+// echo $z."<br>";
+// var_dump($z);
+
+
+// $z=true; ///float Type
+// echo $z."<br>";
+// var_dump($z);
+
+
+// $z=array("html","css","javascript"); ///Array Type
+// echo $z[0]."<br>";
+// var_dump($z);
+
+
+$z=null; ///Null Type
+echo $z."<br>";
+var_dump($z);
+
+?>
